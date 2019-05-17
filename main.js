f17c715becfb4811affb1009fbdd834c4b6e0d93
Carlos Diaz <hola@cjdiaz.com>
2019-05-16 21:18:56 -0500

    Agregando nueva funcion en JS

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-function BeduCoder (nombre, curso){
-  this.nombre =nombre;
+function
+
+
+function BeduCoder(nombre, curso) {
+  this.nombre = nombre;
   this.curso = curso;
 }
 
-BeduCoder.prototype.startCoding(){
+BeduCoder.prototype.startCoding = function() {
+  console.log(
+    `Hola me llamo ${this.nombre} y estoy en el curso de ${this.curso}`
+  );
+};
 
-}
+var helena = new BeduCoder("Helena", "Front-end");
+var carlos = new BeduCoder("Carlos", "Front-end");
+
+helena.startCoding();
